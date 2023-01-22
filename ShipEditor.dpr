72173534afeb57919a66574dcb41fb42195cf085
CryptoTangerine <crypto.tangerine@gmail.com>
2023-01-22 23:18:16 +0100

    engine blocks update

--- ShipEditor.dpr
+++ ShipEditor.dpr
@@ -4,10 +4,10 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  game_functions in '..\spaceships-shared\game_functions.pas',
   game_classes in '..\spaceships-shared\game_classes.pas',
-  game_consts in '..\spaceships-shared\game_consts.pas',
+  game_functions in '..\spaceships-shared\game_functions.pas',
   ship_graphics in '..\spaceships-shared\ship_graphics.pas',
+  game_consts in '..\spaceships-shared\game_consts.pas',
   ship_defaults in '..\spaceships-shared\ship_defaults.pas',
   game_ships in '..\spaceships-shared\game_ships.pas';
 
